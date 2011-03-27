ff20ff7c11d92061390b40eac83404b5b5a81a0b
kjelliver <kjelliver@yahoo.no>
2011-03-27 11:21:11 -0700

    Finished with all accountinfo functionality

--- src/Hulen.BusinessServices/Services/AccountInfoServices.cs
+++ src/Hulen.BusinessServices/Services/AccountInfoServices.cs
@@ -6,14 +6,13 @@ using Hulen.BusinessServices.Interfaces;
 using Hulen.Storage.DTO;
 using Hulen.Storage.Interfaces;
 using Hulen.Storage.Repositories;
-using Microsoft.Office.Interop.Excel;
 
 namespace Hulen.BusinessServices.Services
 {
     public class AccountInfoServices : IAccountInfoServices
     {
         private readonly IAccountInfoRepository _repository = new AccountInfoRepository();
-        private readonly Application _application = new Application();
+        //private readonly Application _application = new Application();
 
 
         public ICollection<AccountInfoDTO> GetAllAccounts()
@@ -56,34 +55,6 @@ namespace Hulen.BusinessServices.Services
             //ms.Position = 0;
 
             //return ms;
-        }
-
-        private ICollection<AccountInfoDTO> ConvertDataSetToAccountInfoObjectCollection(string filepath)
-        {
-            ICollection<AccountInfoDTO> accountInfos = new Collection<AccountInfoDTO>();
-
-            Workbook workbook = _application.Workbooks.Open(filepath);
-            Sheets sheets = workbook.Worksheets;
-            Worksheet worksheet = (Worksheet)sheets.Item["AccountInfo"];
-
-            Range range = worksheet.Range["A1", "F95"];
-            var accountArrey = (Array)range.Cells.Value;
-
-            for (int i = 1; i <= accountArrey.GetLength(0); i++)
-            {
-                if (accountArrey.GetValue(i, 1).ToString() != "")
-                {
-                    var accountInfo = new AccountInfoDTO();
-                    accountInfo.AccountNumber = Convert.ToInt32(accountArrey.GetValue(i, 1).ToString());
-                    accountInfo.AccountName = accountArrey.GetValue(i, 2).ToString();
-                    accountInfo.ResultReportCategory = Convert.ToInt32(accountArrey.GetValue(i, 3).ToString());
-                    accountInfo.PartsReportCategory = Convert.ToInt32(accountArrey.GetValue(i, 4).ToString());
-                    accountInfo.WeekCategory = Convert.ToInt32(accountArrey.GetValue(i, 5).ToString());
-                    accountInfo.IsIncome = Convert.ToBoolean(Convert.ToInt32(accountArrey.GetValue(i, 6).ToString()));
-                    accountInfos.Add(accountInfo);
-                }
-            }
-            return accountInfos;
         }
     }
 }
